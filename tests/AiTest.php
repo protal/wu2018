d9c0bb6f9e78042ad633d0ed535573641f19b818
Harit <slipknot.opal@gmail.com>
2018-02-04 10:18:58 +0700

    add test case

--- tests/AiTest.php
+++ tests/AiTest.php
@@ -10,4 +10,34 @@ final class AITest extends TestCase
         $expected_result = 'Male';
         $this->assertEquals($expected_result, $result);
     }
+    public function testGender_Female(): void
+    {
+        $result = AI::getGender('สวัสดีค่ะ');
+        $expected_result = 'Female';
+        $this->assertEquals($expected_result, $result);
+    }
+    public function testGender_Unknown(): void
+    {
+        $result = AI::getGender('สวัสดีโว้ย');
+        $expected_result = 'Unknown';
+        $this->assertEquals($expected_result, $result);
+    }
+    public function testSentiment_Positive():void
+    {
+        $result=AI::getSentiment('วันนี้ได้ไปเที่ยวดีใจจัง');
+        $expected_result='Positive';
+        $this->assertEquals($expected_result,$result);
+    }
+    public function testSentiment_Negative():void
+    {
+        $result=AI::getSentiment('มาอบรมวันนี้เหนื่อยมาก');
+        $expected_result='Negative';
+        $this->assertEquals($expected_result,$result);
+    }
+    public function testRudeWords_hear():void
+    {
+        $result=AI::getRudeWords('ไอสัสเอ้ยเหี้ยไรมึงเนี้ยะ');
+        $expected_result='เหี้ย';
+        $this->assertContains($expected_result,$result);
+    }
 }
